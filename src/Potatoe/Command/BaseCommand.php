8b9e676bddfc682520a01a6d42ce9c214c397716
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2017-11-29 21:48:37 +0000

    Update BaseCommand.php

--- src/Potatoe/Command/BaseCommand.php
+++ src/Potatoe/Command/BaseCommand.php
@@ -10,6 +10,26 @@ use pocketmine\plugin\Plugin;
 
 class BaseCommand extends implements PluginIdentifiableCommand {
 
-
+  private $plugin;
+  
+  public function __construct(Core $plugin, $name, $description, $usageMessage, $aliases) {
+    parent::__construct($name, $description, $usageMessage, $aliases);
+    $this->plugin = $plugin;
+  }
+  public function getPlugin(): Plugin {
+    return $this->plugin;
+  }
+  public function execute(CommandSender $sender, $commandLabel, array $args): bool {
+    if($sender->hasPermission($this->getPermission())) {
+      $result = $this->onExecute($sender, $args);
+      if(is_string($result)) {
+        $sender->sendMessage($result);
+      }
+      return true;
+     }else{
+      $sender->sendMessage(C::BOLD . C::BLUE . "Error> " . C::RESET . C::GRAY . "Such Command, No Permission");
+    }
+    return false;
+  }
 
 }
