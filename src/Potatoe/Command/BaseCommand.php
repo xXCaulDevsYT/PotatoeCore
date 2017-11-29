f574b31028761cc4dd2150eed519d7529783f65b
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2017-11-29 21:35:58 +0000

    Create BaseCommand.php

---
+++ src/Potatoe/Command/BaseCommand.php
@@ -0,0 +1,15 @@
+<?php
+namespace Potatoe\Command;
+
+use Potatoe\Core;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+
+class BaseCommand extends implements PluginIdentifiableCommand {
+
+
+
+}
