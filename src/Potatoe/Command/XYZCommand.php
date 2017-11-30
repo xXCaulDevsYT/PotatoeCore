e51b88c03797db7564ff40b3ef67bb7f75f363fc
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2017-11-30 15:28:56 +0000

    Update XYZCommand.php

--- src/Potatoe/Command/XYZCommand.php
+++ src/Potatoe/Command/XYZCommand.php
@@ -18,10 +18,7 @@ class XYZCommand extends BaseCommand {
   public function execute(CommandSender $sender, $commandLabel, array $args): bool {
   
     (!isset($args[0])) {
-      $sender->sendMessage("§l§9Coords>§r§7 You're Standing At");
-      $sender->sendMessage("§7 X - " . round($sender->getX()));
-      $sender->sendMessage("§7 Y - " . round($sender->getY()));
-      $sender->sendMessage("§7 Z - " . round($sender->getZ()));
+      $sender->sendMessage("§l§9Coords>§r§7 You're Standing At\n§7 X - " . round($sender->getX())"\n§7 Y - " . round($sender->getY())"\n§7 Z - " . round($sender->getZ()));
     }
    }else{
      $sender->sendMessage(Core::USE_IN_GAME);
