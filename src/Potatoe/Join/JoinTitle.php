17a37f840a9bd5edd1290c8a3a02a522ca4185d7
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2017-11-30 08:19:15 +0000

    Create JoinTitle.php

---
+++ src/Potatoe/Join/JoinTitle.php
@@ -0,0 +1,19 @@
+<?php
+namespace Potatoe\Join;
+
+use Potatoe\Core
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class JoinTitle implements Listener 
+
+  public function onJoin(PlayerJoinEvent $event) {
+    $player = $event->getPlayer();
+    $title = "Welcome";
+    $subtitle = "To The Server";
+    $player->addTitle($title, $subtitle);
+  }
+}
