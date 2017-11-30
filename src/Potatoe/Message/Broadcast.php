76d5a427c445e2ce8d7320edfa290bb4383cf674
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2017-11-30 08:12:08 +0000

    Create Broadcast.php

---
+++ src/Potatoe/Message/Broadcast.php
@@ -0,0 +1,21 @@
+<?php
+namespace Potatoe\Message;
+
+use Potatoe\Core;
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat as C;
+
+class Broadcast extends PluginTask {
+  
+  public function onRun(/*int*/ $currentTick) {
+    $input = [
+      "§l§9Tip> §r§7Message 1",
+      "§l§9Tip> §r§7Message 2",
+      "§l§9Tip> §r§7Message 3"
+    ];
+    $details = array_rand($input);
+    Server::getInstance()->broadcastMessage(C::GRAY . $input[$details]);
+  }
+}
