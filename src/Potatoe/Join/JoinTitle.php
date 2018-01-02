5ffaa766e3bfed80f45e9bd7660b8a7ffb20d96b
Virvolta <33796736+Virvolta@users.noreply.github.com>
2018-01-02 01:17:24 +0100

    add guardian packet

--- src/Potatoe/Join/JoinTitle.php
+++ src/Potatoe/Join/JoinTitle.php
@@ -5,6 +5,7 @@ use Potatoe\Core
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
 use pocketmine\Player;
 use pocketmine\Server;
 
@@ -14,6 +15,11 @@ class JoinTitle implements Listener {
     $player = $event->getPlayer();
     $title = "Welcome";
     $subtitle = "To The Server";
+    $pk = new LevelEventPacket();
+		$pk->evid = LevelEventPacket::EVENT_GUARDIAN_CURSE;
+		$pk->data = 1;
+		$pk->position = $this->player->asVector3();
+		$this->player->dataPacket($pk);
     $player->addTitle($title, $subtitle);
   }
 }
