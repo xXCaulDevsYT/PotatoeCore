275199f0ec94074ed407ad4523b49926c0a401ff
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-06-09 20:02:22 -0500

    Update JoinMessage.php

--- src/Potatoe/join/JoinMessage.php
+++ src/Potatoe/join/JoinMessage.php
@@ -13,7 +13,13 @@ class JoinMessage implements Listener{
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
-        $player->sendMessage("Welcome To The Server");
-        $player->sendMessage("Core Made By Potatoe");
+        $player->sendMessage("§l§e--------------------§6====================§r");
+        $player->sendMessage("§l§c >§bWelcome To §dC§5u§ab§2e§7X§r");
+        $player->sendMessage("§l§b --§9 VOTE§r: https://votelink");
+        $player->sendMessage("§l§a --§5 STORE§r: Store Link");
+        $player->sendMessage("§l§d --§e DISCORD§r: Discord Link");
+        $player->sendMessage("§l§3 --§2 NEWS§r: §7Plugin Made By EmeraldAssasinYT");
+        $player->sendMessage("§l§7 --§1 ServerIP§r: play.cubexmcpe.com");
+        $player->sendMessage("§l§e--------------------§6====================§r");
     }
 }
