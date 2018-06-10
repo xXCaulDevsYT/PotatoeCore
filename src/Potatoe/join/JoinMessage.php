9d3e9353827e9bbab15134c9e009d8aec82088a2
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-06-10 18:13:56 -0500

    Update JoinMessage.php

--- src/Potatoe/join/JoinMessage.php
+++ src/Potatoe/join/JoinMessage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Potatoe\join;
 
 use Potatoe\Core;
+
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 
@@ -12,7 +13,15 @@ class JoinMessage implements Listener{
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
-        $player->sendMessage("§d--------------§b=============");
-        $player->sendMessage("§aActivated Plugin§e Core by EmeraldassasinYT");
+        $player->sendMessage("§l§6» §aWelcome To §bCube§dX §aNetwork §c!");
+        $player->sendMessage("§e- §3Twitter: <Vote Link>");
+        $player->sendMessage("§e- §3Store: <buycraft link>");
+        $player->sendMessage("§e- §3Discord: <discord link>");
+        $player->sendMessage("§e- §3Hub: <play.cubexmcpe.com");
+        $player->sendMessage("§e- §bCheck our rules with /rules");
+        $player->sendMessage("§e- §bTrade In vote Points For Prizes");
+        $player->sendMessage("");
+        $player->sendMessage("§bCube§cX §aAuthentcation §l§6» §r§7 You Have Successfully Been");
+        $player->sendMessage("§7Logged In By Your Personal IP Adress");
     }
 }
