860a663858c09fb6f762a8c6651ea9b7d3993479
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-06-09 20:04:50 -0500

    Update JoinMessage.php

--- src/Potatoe/join/JoinMessage.php
+++ src/Potatoe/join/JoinMessage.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Potatoe\join;
 
 use Potatoe\Core;
-
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 
@@ -13,7 +12,7 @@ class JoinMessage implements Listener{
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
-        $player->sendMessage("Welcome To The Server");
-        $player->sendMessage("Core Made By Potatoe");
+        $player->sendMessage("§d--------------§b=============");
+        $player->sendMessage("§aActivated Plugin§e Core by EmeraldassasinYT");
     }
 }
