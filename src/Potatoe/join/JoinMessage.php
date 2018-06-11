9946d127c0ad3a938e2b29793d2b5f89f84a51fe
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-06-10 19:20:45 -0500

    Update JoinMessage.php

--- src/Potatoe/join/JoinMessage.php
+++ src/Potatoe/join/JoinMessage.php
@@ -13,15 +13,21 @@ class JoinMessage implements Listener{
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
+        $player->sendMessage("§€-");
         $player->sendMessage("§l§6» §aWelcome To §bCube§dX §aNetwork §c!");
-        $player->sendMessage("§e- §3Twitter: <Vote Link>");
-        $player->sendMessage("§e- §3Store: <buycraft link>");
-        $player->sendMessage("§e- §3Discord: <discord link>");
-        $player->sendMessage("§e- §3Hub: <play.cubexmcpe.com");
+        $player->sendMessage("§e- §3Twitter: <>");
+        $player->sendMessage("§e- §3Store: <>");
+        $player->sendMessage("§e- §3Discord: <https://discord.gg/XpvRssz>");
+        $player->sendMessage("§e- §3Hub: <play.cubexmcpe.com>");
         $player->sendMessage("§e- §bCheck our rules with /rules");
         $player->sendMessage("§e- §bTrade In vote Points For Prizes");
-        $player->sendMessage("");
+        $player->sendMessage("§€-");
         $player->sendMessage("§bCube§cX §aAuthentcation §l§6» §r§7 You Have Successfully Been");
         $player->sendMessage("§7Logged In By Your Personal IP Adress");
+        $player->sendMessage("§€-");
+        $player->sendMessage("§e- §6Make Sure to Read are Rules before playing!");
+        $player->sendMessage("§e- §6Voting Gets You Alot Of Items & Cool Gadgets!");
+        $player->sendMessage("§e- §6Donating Rewards you Will Cool Perks / Abilities & Features");
+        $player->sendMessage("§€-");
     }
 }
