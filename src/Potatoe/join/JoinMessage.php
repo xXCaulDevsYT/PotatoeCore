21919ef500cdc71412837caad85095170e117ba0
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-09-28 13:50:27 -0500

    Update JoinMessage.php

--- src/Potatoe/join/JoinMessage.php
+++ src/Potatoe/join/JoinMessage.php
@@ -13,21 +13,11 @@ class JoinMessage implements Listener{
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
-        $player->sendMessage("§€");
-        $player->sendMessage("§l§6» §aWelcome To §bDivinium§dMC §aNetwork §c!");
-        $player->sendMessage("§e- §3Vote: <>");
-        $player->sendMessage("§e- §3Store: <>");
-        $player->sendMessage("§e- §3Discord: <https://discord.gg/bpfjN25>");
-        $player->sendMessage("§e- §3Playing: <FactionsOP 1>");
-        $player->sendMessage("§e- §bCheck our rules with /rules");
-        $player->sendMessage("§e- §bTrade In vote Points For Prizes");
-        $player->sendMessage("§€");
-        $player->sendMessage("§bDivinium§cMC §aIP Logger §l§6» §r§7 You Have Successfully Been");
-        $player->sendMessage("§7Logged By Your Personal IP Address");
-        $player->sendMessage("§€");
-        $player->sendMessage("§e- §6Make Sure to Read are Rules before playing!");
-        $player->sendMessage("§e- §6Voting Gets You Alot Of Items & Cool Gadgets!");
-        $player->sendMessage("§e- §6Donating Rewards you Will Cool Perks / Abilities & Features");
-        $player->sendMessage("§€");
+        $player->sendMessage("§l§3----------§cAurora§5MC§3----------");
+        $player->sendMessage("§bServer§7: §fPvPAurora.ddns.net");
+        $player->sendMessage("§bStore§7: §fauroramcpe.buycraft.net");
+        $player->sendMessage("§bDiscord§7: §fhc2NqwT");
+        $player->sendMessage("§bRules§7: §f/rules");
+        $player->sendMessage("§l§3----------------------------------");
     }
 }
